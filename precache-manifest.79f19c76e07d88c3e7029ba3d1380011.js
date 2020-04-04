0c1f8a3698c497fb6d2fd7c6f0a16afb93217382
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2020-04-04 12:32:14 -0400

    Updates

---
+++ precache-manifest.79f19c76e07d88c3e7029ba3d1380011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae34d30c0424f480c7e9f8c38e6efafb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ad0a8a3f92d907af10",
+    "url": "/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "a3cfec3aa7ad2fe52674",
+    "url": "/static/css/main.7365bbd1.chunk.css"
+  },
+  {
+    "revision": "18ad0a8a3f92d907af10",
+    "url": "/static/js/2.be4f8f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be4f8f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cfec3aa7ad2fe52674",
+    "url": "/static/js/main.270caf64.chunk.js"
+  },
+  {
+    "revision": "e00cacda94403fa8808c",
+    "url": "/static/js/runtime-main.61eed8fe.js"
+  }
+]);
