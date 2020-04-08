db5d9f43658b6820f38850d0671e57c71718b73a
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2020-04-08 12:44:22 -0400

    Updates

--- precache-manifest.d9d6fab7b6e766b3973329497e359773.js
+++ precache-manifest.d9d6fab7b6e766b3973329497e359773.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db6aedf2ff15d97682f81c0c08df9f69",
+    "revision": "fb2d1031eece46df7682f9debfed658b",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b9b2dd63.chunk.css"
   },
   {
-    "revision": "1d2848bdde9c17c6606b",
-    "url": "/static/css/main.1ed45a96.chunk.css"
+    "revision": "3ba03768bf95885fea28",
+    "url": "/static/css/main.663fea32.chunk.css"
   },
   {
     "revision": "5f85db827ce602870e4c",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.017c7a02.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d2848bdde9c17c6606b",
-    "url": "/static/js/main.66c58508.chunk.js"
+    "revision": "3ba03768bf95885fea28",
+    "url": "/static/js/main.2d1a2658.chunk.js"
   },
   {
     "revision": "e00cacda94403fa8808c",
